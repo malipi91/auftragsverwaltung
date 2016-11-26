03f83249cb09140664c99f1064f71682c4b54eff
mehmetimpram\mehmet_impram <mehmet_impram@live.de>
2016-11-26 15:08:03 +0100

    Artikel ID Ausgegeraut man darf es nicht bearbeiten

--- src/view/ArtikelBearbeiten.java
+++ src/view/ArtikelBearbeiten.java
@@ -19,6 +19,8 @@ public class ArtikelBearbeiten extends javax.swing.JInternalFrame {
         
         //Mechmet Impram
         jtfartikelid_ArtikelBearbeiten.setDocument(new UniversalDokument(false, true, "", 5));
+        //Artikel Id kann man nicht bearbeiten
+        jtfArtikelID_ArtikelBearbeiten_Gegraut.setEnabled(false);
 
         
     }
