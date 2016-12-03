c1eed22cf2849533272becff3862e38acb03c552
Samet Sahin <sametsahin1991@googlemail.com>
2016-12-03 01:50:16 +0100

    Auftrag TEst weitere bearbeitung

--- test/dao/DAOAuftragTest.java
+++ test/dao/DAOAuftragTest.java
@@ -26,40 +26,39 @@ public class DAOAuftragTest {
     
     @Before
     public void setUp() throws SQLException {
-        Auftrag auftrag = new Auftrag("9999901","test", "1480600612165", "1480600612165", "bar", 4344,"f", "1480600612165");
+        
         
         da = new DAOAuftrag();
-        da.legeNeueAuftragAn(auftrag);
+        
     }
     
     
-//    @Test
-//    public void testErhalteAuftragFuerID() throws Exception {
-//        Auftrag ergebnis =  da.erhalteAuftragFuerID("8");
-//        assertEquals("8", ergebnis.getAuftrags_ID());
-//        
-//    }
-//    
-//    @Test 
-//    public void testLegeNeuenAuftragAn() throws SQLException{
-//        Auftrag auftrag2 = new Auftrag("99999","test", "1480600612165", "1480600612165", "bar", 100,"f", "1480600612165");
-//        da.legeNeueAuftragAn(auftrag2);
-//        assertEquals("9",da.erhalteAuftragFuerID("9").getAuftrags_ID());
-//    }
+    @Test
+    public void testErhalteAuftragFuerID() throws Exception {
+        Auftrag ergebnis =  da.erhalteAuftragFuerID("98");
+        assertEquals("98", ergebnis.getAuftrags_ID());
+        
+    }
     
-    
+    @Test 
+    public void testLegeNeuenAuftragAn() throws SQLException{
+        Auftrag auftrag = new Auftrag("129","test", "1480600612165", "1480600612165", "bar", 4344,"f", "1480600612165");     
+        da.legeNeueAuftragAn(auftrag);
+        assertEquals("129",da.erhalteAuftragFuerID("129").getAuftrags_ID());
+    }
     
     
     @Test
     public void testLöscheAuftrag() throws SQLException{
-        da.loescheAuftrag("93");
-        assertEquals(da.erhalteLKZAuftragFuerID("93").getLKZ(),"w");
+        
+        da.loescheAuftrag("129");
+        assertEquals(da.erhalteLKZAuftragFuerID("129").getLKZ(),"w");
         
     }
     
-//    @Test
-//    public void testGibAlleAuftraege() throws SQLException{
-//        assertEquals(9, da.gibAlleAuftraege(5).get(3));          
-//    }
-//    
+    @Test
+    public void testGibAlleAuftraege() throws SQLException{
+        assertEquals(9, da.gibAlleAuftraege(5).get(3));          
+    }
+    
 }
