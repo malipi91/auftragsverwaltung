77bd5ccd12f5e01b70238e583e3675954437e4a8
Martin Lipinski <martin.lipinski@studmail.w-hs.de>
2016-12-05 11:53:34 +0100

    Konflikte behoben

--- test/dao/DAOArtikelTest.java
+++ test/dao/DAOArtikelTest.java
@@ -77,7 +77,7 @@ public class DAOArtikelTest {
         System.out.println("loescheArtikel");
         String id = "";
         DAOArtikel instance = new DAOArtikel();
-        instance.loescheArtikel(id);
+        instance.loescheArtikelMitID(id);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
